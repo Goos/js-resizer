ff0f777b4e2ce52d2227c23a5f7c051a76f85964
Robin Goos <robingoos@gmail.com>
2013-01-21 20:23:26 +0100

    Added functionality to scan for 'media-classes'

--- resizer.js
+++ resizer.js
@@ -29,9 +29,11 @@
 (function(Resizer, $, window) {
     "use strict";
 
-    Resizer.listeners = [];
+    Resizer.listeners       = [];
+    Resizer.currentMedia    = "desktop";
 
-    // Main attach-method
+    // Main attacher
+    /* Arguments: sizes | callback | listenerType | $context */
     Resizer.addSizeListener = function(args, $context) {
         if (typeof args.inside !== "function" && !(args.sizes instanceof Array || typeof args.sizes === "number")) {
             console.log("No listener added: Bad parameters");
@@ -43,8 +45,7 @@
         listener.sizeSpan = {};
 
         if (typeof args.sizes === "number") {
-        // Adds size span max to maximum possible integer when only a min value is specified, 
-        // because who knows what magic the future will bring?
+        // Adds size span max to maximum possible integer when only a min value is specified
             listener.type = type;
             listener.sizeSpan.min = args.sizes;
             listener.sizeSpan.max = 9007199254740992;
@@ -62,11 +63,12 @@
         Resizer.listeners.push(listener);
     }
 
-    // jQuery attach-method
+    // jQuery event attacher
     $.fn.addSizeListener = function(args) {
         function init() {
             Resizer.addSizeListener(args, this);
             var $obj = $(this);
+            $obj.off('$resize');
             // Setting a custom event, in order to easily trigger the resize
             $obj.on('$resize', args.inside);
             
@@ -78,10 +80,17 @@
     // Actual resize-listener
     Resizer.runListeners = function(event) {
         var height = $(window).height(), width = $(window).width();
+        if (width > 0 && width <= 499) {
+            Resizer.currentMedia = "mobile";
+        } else if (width > 499 && width <= 799) {
+            Resizer.currentMedia = "tablet";
+        } 
+        if (width > 499) {
+            Resizer.currentMedia = "desktop";
+        }
 
         $.each(Resizer.listeners, function(index, listener) {
-            // Not entirely sure what listening separately to width/height brings to the table,
-            // candidate for removal?
+            // Listen to width and height seperately
             if (listener.type === "width") {
                 if (listener.sizeSpan.min <= width && listener.sizeSpan.max >= width) {
                     // using function.call() if the listener has provided a context to call to (in case of jQuery binding)
@@ -113,6 +122,76 @@
                 }
             }
             
+        });
+    }
+
+/**
+ *  Makes sure the data-classes don't conflict
+ *  (e.g: if an element has "hidden" as both mobile & tablet-class)
+ */
+    function checkConflicts ($obj, classes, media) {
+        var classes = classes.split(' '),
+            validClasses = [];
+        $.each(classes, function (i, aClass) {
+            var regexp = new RegExp(aClass),
+                previousClasses = $obj.attr('data-'+media+'class');
+            if (!regexp.test(previousClasses)) {
+                validClasses.push(aClass);
+            }
+        });
+        return (validClasses.length > 0) ? validClasses.join(' ') : false;
+    }
+
+/**
+ * Below courtesy of Fredrik Söderquist, https://github.com/fregu
+ * Scans the DOM after elements with the data-[media]class attribute
+ * and adds the classes specified by the media, when within its
+ * bounds
+ * 
+ * @param  {jQuery} container The jQuery-element to search within
+ */
+    Resizer.scanClasses = function (container) {
+        $('[data-mobileclass]', container).each(function () {
+            var $obj = $(this);
+            $obj.addSizeListener({
+                listenerType: "width", 
+                sizes   : [0, 499],
+                inside      : function () {
+                    $obj.addClass($obj.attr('data-mobileclass'));
+                },
+                outside      : function () {
+                    var validClasses = checkConflicts($obj, $obj.attr('data-mobileclass'), Resizer.currentMedia);
+                    $obj.removeClass(validClasses);
+                }
+            });
+        });
+        $('[data-tabletclass]', container).each(function (){
+            var $obj = $(this);
+            $obj.addSizeListener({
+                listenerType: "width", 
+                sizes   : [499, 799],
+                inside      : function () {
+                    $obj.addClass($obj.attr('data-tabletclass'));
+                },
+                outside      : function () {
+                    var validClasses = checkConflicts($obj, $obj.attr('data-tabletclass'), Resizer.currentMedia);
+                    $obj.removeClass(validClasses);
+                }
+            });
+        });
+        $('[data-desktopclass]', container).each(function (){
+            var $obj = $(this);
+            $obj.addSizeListener({
+                listenerType: "width", 
+                sizes   : 499,
+                inside      : function () {
+                    $obj.addClass($obj.attr('data-desktopclass'));
+                },
+                outside      : function () {
+                    var validClasses = checkConflicts($obj, $obj.attr('data-desktopclass'), Resizer.currentMedia);
+                    $obj.removeClass(validClasses);
+                }
+            });
         });
     }
 
